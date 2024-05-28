60cf874f7e9603da831d21e7fc45107618dfa41e
Adriano Matos <adrianotaua@hotmail.com>
2024-05-28 11:28:54 -0300

    Criação do HTML e CSS para o Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <div className="card">
+        <h1>Java</h1>
+        <img
+          src="https://salvatore.academy/devmon/1_java.png"
+          alt=""
+          width={"100px"}
+        />
+      </div>
+      <br />
+
+      <div className="card">
+        <h1>Kotlin</h1>
+        <img
+          src="https://salvatore.academy/devmon/2_kotlin.png"
+          alt=""
+          width={"100px"}
+        />
+      </div>
+      <br />
+
+      <div className="card">
+        <h1>Android</h1>
+        <img
+          src="https://salvatore.academy/devmon/3_android.png"
+          alt=""
+          width={"100px"}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
