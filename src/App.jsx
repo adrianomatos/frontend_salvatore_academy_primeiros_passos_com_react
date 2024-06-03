3c9a4a43c09cbd41a91e20bfae770f82a72125c7
Adriano Matos <adrianotaua@hotmail.com>
2024-06-03 18:29:55 -0300

    Criação do Componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import "./App.css";
+// import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h1>Java</h1>
-        <img
-          src="https://salvatore.academy/devmon/1_java.png"
-          alt=""
-          width={"100px"}
-        />
-      </div>
-      <br />
-
-      <div className="card">
-        <h1>Kotlin</h1>
-        <img
-          src="https://salvatore.academy/devmon/2_kotlin.png"
-          alt=""
-          width={"100px"}
-        />
-      </div>
-      <br />
-
-      <div className="card">
-        <h1>Android</h1>
-        <img
-          src="https://salvatore.academy/devmon/3_android.png"
-          alt=""
-          width={"100px"}
-        />
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </>
   );
 }
-
 export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,31 @@
+import "./Card.css";
+export default function Card() {
+  return (
+    <div className="card">
+      <h1>Pokemon Java</h1>
+      <img
+        src="https://salvatore.academy/devmon/1_java.png"
+        alt=""
+        width={"100px"}
+      />
+    </div>
+
+    // <div className="card">
+    //   <h1>Kotlin</h1>
+    //   <img
+    //     src="https://salvatore.academy/devmon/2_kotlin.png"
+    //     alt=""
+    //     width={"100px"}
+    //   />
+    // </div>
+
+    // <div className="card">
+    //   <h1>Android</h1>
+    //   <img
+    //     src="https://salvatore.academy/devmon/3_android.png"
+    //     alt=""
+    //     width={"100px"}
+    //   />
+    // </div>
+  );
+}
